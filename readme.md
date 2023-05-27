306181857cfdbd2bd45255ae2330d7700f18b88a
BrunoAndadeDinis <bruno.a.dinis96@gmail.com>
2023-05-27 03:00:59 -0300

    Atualização Index v2.0 + readme básico

---
+++ readme.md
@@ -0,0 +1,10 @@
+<h1 align="center">Curso de Programação web</h1>
+
+<h3 align="center">Tecnologias utilizadas:</h3>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+</ul>
+
+<p>Curso de introdução a programação web, que traz conhecimentos fundamentais em HTML, CSS e Javascript.</p>
---
+++ script.js
@@ -0,0 +1,25 @@
+var fechar = document.querySelector("input");
+var iframeContainer = document.getElementById("iframeContainer");
+
+fechar.addEventListener("click", function () {
+    iframeContainer.style.display = "none";
+});
+
+var anchorList = document.querySelectorAll('ul li a');
+var iframe = document.querySelector("iframe")
+console.log(iframe)
+// Adiciona um evento de clique em cada âncora
+anchorList.forEach(function (anchor) {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault(); // Impede o comportamento padrão do clique no link
+
+        var href = event.target.href; // Obtém o valor do atributo href
+        console.log(href)
+        iframe.src = href; // Atribui o valor do atributo href ao atributo src do iframe
+
+        iframeContainer.style.display = "block";
+
+        let completa = document.querySelector("a")
+        completa.href = href 
+    });
+});
